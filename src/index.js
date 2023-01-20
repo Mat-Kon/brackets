3e35f2a46f8db9e327527f63c330fd24536c4f50
Mat-Kon <mat_kon@mail.ru>
2023-01-20 12:30:07 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length === 0){
+    return false;
+  }
+
+  let bracket = bracketsConfig.join('');
+  
+  let a = '';
+  let b ='';
+
+  for (let i = 0; i < str.length; i++){
+    a = str[i];
+
+    for(let j = 0; j < bracket.length; j++){
+      b = bracket[j];
+    }
+
+    if (a === b) {
+      return true
+    }else{
+      return false
+    }
+        
+  }
+
+
+
+
+  
 }
